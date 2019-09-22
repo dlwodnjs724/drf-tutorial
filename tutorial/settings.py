f005ea55c11d3e78538f21b18242b44f757fd21b
이재원 <dlwodnjs724@naver.com>
2019-09-23 03:06:57 +0900

    tutorial 5, done

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -124,7 +124,6 @@ STATIC_URL = '/static/'
 
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.renderers.JSONRenderer',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
